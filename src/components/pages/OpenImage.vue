c993f135c34502cb1280e2d8fcf26815751620aa
Enzo Innocenzi <enzo@innocenzi.dev>
2021-05-12 01:03:34 +0200

    refactor: update loading page design

--- src/components/pages/OpenImage.vue
+++ src/components/pages/OpenImage.vue
@@ -1,52 +1,7 @@
 <template>
 	<modal :show="show" @close="close">
 		<section class="flex flex-col items-center justify-center pointer-events-auto w-full max-w-lg">
-			<!-- Dotted box -->
-			<div class="flex flex-col w-full max-w-lg">
-				<!-- Input -->
-				<label
-					for="file-upload"
-					:class="[
-						'flex items-center justify-center',
-						'border-dashed rounded border-gray-600 border-3 shadow-xl bg-gray-800',
-						'h-full w-full p-6 min-h-64',
-						'cursor-pointer',
-						'hover:border-gray-500',
-						'focus:border-pink-300',
-						'focus-within:border-pink-300',
-						'transform transition'
-					]"
-				>
-					<div class="space-y-2 text-center">
-						<svg
-							class="mx-auto text-gray-400 h-14 w-14"
-							stroke="currentColor"
-							fill="none"
-							viewBox="0 0 48 48"
-							aria-hidden="true"
-						>
-							<path
-								d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-								stroke-width="2"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-						</svg>
-						<div class="flex">
-							<div class="relative transition font-medium text-pink-300 rounded-md cursor-pointer hover:text-pink-400 focus-within:outline-none">
-								<span>Upload a file</span>
-								<input id="file-upload" name="file-upload" type="file" class="sr-only" @change="onFileInput" />
-							</div>
-							<p class="pl-1">or drag and drop</p>
-						</div>
-					</div>
-				</label>
-
-				<!-- Error -->
-				<span v-if="errors.fileInput" class="mt-2 text-sm text-red-300" v-text="errors.fileInput"></span>
-			</div>
-
-			<span class="my-6 text-gray-400">or</span>
+			<img src="/logo.svg" class="w-24 h-24 mb-10" alt="Logo">
 
 			<!-- Manual link -->
 			<div class="flex flex-col w-full max-w-lg">
@@ -88,6 +43,53 @@
 
 				<!-- Error -->
 				<span v-if="errors.urlInput" class="mt-2 text-sm text-red-300" v-text="errors.urlInput"></span>
+			</div>
+
+			<span class="my-6 text-gray-400">or</span>
+
+			<!-- Dotted box -->
+			<div class="flex flex-col _w-full max-w-lg">
+				<!-- Input -->
+				<label
+					for="file-upload"
+					:class="[
+						'flex items-center justify-center',
+						'_border-dashed rounded border-gray-700 border-1 shadow-sm bg-gray-800',
+						'h-full _w-full py-2.5 px-12 _min-h-64',
+						'cursor-pointer',
+						'hover:border-gray-500',
+						'_focus:border-pink-300',
+						'_focus-within:border-pink-300',
+						'transform transition'
+					]"
+				>
+					<div class="flex items-center space-x-2 text-center">
+						<svg
+							class="mx-auto text-gray-400 h-6 w-6"
+							stroke="currentColor"
+							fill="none"
+							viewBox="0 0 48 48"
+							aria-hidden="true"
+						>
+							<path
+								d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+						<div class="flex">
+							<div class="relative transition text-sm font-medium text-gray-400 group-hover:text-gray-300 rounded-md cursor-pointer hover:text-pink-400 focus-within:outline-none">
+								<span>Upload a file</span>
+								<input id="file-upload" name="file-upload" type="file" class="sr-only" @change="onFileInput" />
+							</div>
+							<!-- <p class="pl-1">or drag and drop</p> -->
+						</div>
+					</div>
+				</label>
+
+				<!-- Error -->
+				<span v-if="errors.fileInput" class="mt-2 text-sm text-red-300" v-text="errors.fileInput"></span>
 			</div>
 		</section>
 	</modal>
--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,32 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import windi from 'vite-plugin-windicss'
 import components from 'vite-plugin-components'
 import fonts from 'vite-plugin-fonts'
 import vue from '@vitejs/plugin-vue'
-import icons, { ViteIconsResolver }  from 'vite-plugin-icons'
-import path from 'path'
+import icons, { ViteIconsResolver } from 'vite-plugin-icons'
 
 export default defineConfig({
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, 'src')
-		}
+			'@': path.resolve(__dirname, 'src'),
+		},
 	},
-  plugins: [
+	plugins: [
 		vue(),
 		windi(),
 		components({
 			customComponentResolvers: ViteIconsResolver({
-				componentPrefix: ''
-			})
+				componentPrefix: '',
+			}),
 		}),
 		icons(),
-    fonts({
-      google: {
-        families: [
-					{ name: 'Inter', styles: 'wght@400;500;600' }
-				]
-      },
-    })
-	]
+		fonts({
+			google: {
+				families: [
+					{ name: 'Inter', styles: 'wght@200;400;500;600' },
+				],
+			},
+		}),
+	],
 })
