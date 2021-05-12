ec4f1426400c46a1dcf3f3744848b5729262eb89
Enzo Innocenzi <enzo@innocenzi.dev>
2021-05-12 14:44:03 +0200

    fix: improve footer focus style

--- src/components/FooterLink.vue
+++ src/components/FooterLink.vue
@@ -1,3 +1,3 @@
 <template>
-	<a class="transition hover:text-pink-300"><slot /></a>
+	<a class="transition hover:text-pink-300 focus:underline focus:outline-none focus:text-pink-400"><slot /></a>
 </template>
