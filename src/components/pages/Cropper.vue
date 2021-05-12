f693ae3ee6bfc8672672772d1b3834af7cc1ba40
Enzo Innocenzi <enzo@innocenzi.dev>
2021-05-12 14:34:27 +0200

    fix: improve footer responsiveness

--- src/components/pages/Cropper.vue
+++ src/components/pages/Cropper.vue
@@ -28,12 +28,13 @@
 
 		<section class="flex flex-col items-center flex-shrink-0 w-full px-6">
 			<toolbar />
-			<footer class="flex items-center justify-center p-8 space-x-8 text-xs text-gray-500">
+			<footer class="flex text-center items-center justify-center p-8 space-x-8 text-xs text-gray-500">
 				<footer-link href="https://github.com/innocenzi/avatar">
 					GitHub
 				</footer-link>
 				<span>
-					Made with <span class="text-[0.7rem]">❤</span> by
+					<span class="hidden sm:inline">Made with <span class="text-[0.7rem]">❤</span>&nbsp;</span>
+					<span>by&nbsp;</span>
 					<footer-link class="font-bold" href="https://twitter.com/enzoinnocenzi">Enzo Innocenzi</footer-link>
 				</span>
 				<footer-link href="https://github.com/innocenzi/avatar/issues">
